bdf4e11297a3ef29e68d33623b5237ecd1d27a4c
Jatin <jatinjayadev@gmail.com>
2024-03-14 16:13:56 +0530

    Connected Database using Mongoose

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const mongoose = require('mongoose')
+require("dotenv").config()
 
-app.get('/', (req, res) => {
-    res.redirect('/ping');
-});
+const URI = process.env.MONGODB_URI
 
+mongoose.connect(URI)
+    .then(() => {
+        console.log("Connected to MongoDB")
+        app.get('/', (req, res) => {
+            res.redirect('/ping');
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 app.get('/ping', (request,response) => {
-    response.send("This is from S60_PickUp_Lines")
-})
+    
+    const isConnected = mongoose.connection.readyState === 1;
+    
+    // Send response with database connection status
+    response.json({
+        database: isConnected ? 'connected' : 'disconnected',
+    });
+
+});
 
 app.listen(port, () => {
     console.log("This is from port 4000")
