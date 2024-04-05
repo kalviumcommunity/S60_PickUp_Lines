5dc040d3ad26ed5c01712534f27d0dd45db97fcf
Jatin <jatinjayadev@gmail.com>
2024-04-05 14:20:17 +0530

    Created React

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <center>
+        <h2>The idea is to create a platform for pickup lines where users can browse or add a collection of pickup lines.</h2>
+      </center>
+    </div>
+  )
+}
+
+export default App
