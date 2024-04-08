b36f04d892035df21864694ba9e93f48d424ad2d
Jatin <jatinjayadev@gmail.com>
2024-04-08 10:47:23 +0530

    Added Dummydata

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import data from "./Components/data.json"
 
 function App() {
 
   return (
-    <div>
-      <center>
-        <h2>The idea is to create a platform for pickup lines where users can browse or add a collection of pickup lines.</h2>
-      </center>
+    <div className='container'>
+      {data.map((data) => {
+        return (
+          <div className='data-container' key={data.id}>
+            {data.lines}
+            <div className='category' >
+              <p>Category: {data.category}</p>
+              <p>{data.likes} ❤</p>
+            </div>
+          </div>
+        )
+      })}
+
     </div>
   )
 }
