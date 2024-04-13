0ccb5963dfbbfbb8abde042a041360d2558459d2
Jatin <jatinjayadev@gmail.com>
2024-04-13 10:20:14 +0530

    Implemented Update and Delete Method

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import NavBar from './Components/NavBar'
 import PickUpLines from './Components/PickUpLines'
 import AddLines from './Components/AddLines';
+import UpdateLines from './Components/UpdateLines';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<PickUpLines />} />
         <Route path='/AddPickUpLine' element={<AddLines />} />
+        <Route path='/updateLine/:id' element={<UpdateLines />} />
       </Routes>
     </div>
   );
